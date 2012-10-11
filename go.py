f9331b61e8b9d32151c56229a10ab6dc9914b737
mog33 <ctrl.alt.del@gmx.net>
2012-10-11 23:35:08 +0200

    restructured to class; allows multiple repositories

--- go.py
+++ go.py
@@ -17,91 +17,81 @@ except:
     print "Make sure 'pysvn' and 'pynotify' is installed."
     exit()
 
-import datetime, time, ConfigParser as cfg
+import datetime, time, os, ConfigParser as cfg
 
-def read_config():
-    """Read the configuration file containing server, username and password"""
-    global svn_root, svn_username, svn_password
+class SvnNotifier():
 
-    config_file = 'svnnotify.cfg'
-    config_header = 'server'
+    def __init__(self, name, svn_root, svn_username, svn_password):
+        self.name = name
+        self.svn_root = svn_root
+        self.svn_username = svn_username
+        self.svn_password = svn_password
+        self.last_revision = pysvn.Revision(pysvn.opt_revision_kind.number, 0)
+        self.client = pysvn.Client()
+        self.client.callback_get_login = self.credentials
 
+    def notify(self, author, date, revision, message, paths):
+        """Display the changed paths using libnotify"""
+        title_string = 'New commit #%s to repository %s' % (revision, self.name)
+        message_string = "[%s] %s: %s\n Paths: %s" % \
+                        (date.strftime("%d.%m %H:%M"), author, message, paths)
+        pynotify.Notification(title_string, message_string, "view-refresh").show()
+
+    def credentials(self, realm, username, may_save):
+        """Return the default login credentials"""
+        return True, self.svn_username, self.svn_password, False
+
+    def discover_changes(self):
+        """
+        Find out the changes occured since the last time this method is ran
+        """
+        log = self.client.log(
+            self.svn_root,
+            discover_changed_paths=True,
+            revision_end=self.last_revision
+            )
+        log = log[:-1]   # Ignore last revision
+        if len(log) > 0:
+            self.last_revision = log[0].revision
+            if len(log) > 3:
+                pynotify.Notification("Older Commits for repository %s available" % self.name, None, "view-refresh").show()
+                log = log[0:3]
+            log.reverse()
+            for entry in log:
+                author = entry.get('author')
+                rev = entry.revision.number
+                message = entry.message
+                date = datetime.datetime.fromtimestamp(entry.date)
+                paths = []
+                for change in entry.changed_paths:
+                    if change.path not in paths:
+                        paths.append(change.path)
+                self.notify(author, date, rev, message, paths)
+
+
+def main():
+    path = os.path.dirname(__file__)
+    config_file = os.path.join(path, 'svnnotify.cfg')
+    repos = []
     try:
         parser = cfg.ConfigParser()
         parser.read(config_file)
-        svn_root = parser.get(config_header, 'server')
-        svn_username = parser.get(config_header, 'user')
-        svn_password = parser.get(config_header, 'pass')
+        for section in parser.sections():
+            server = parser.get(section, 'server')
+            user = parser.get(section, 'user')
+            passw = parser.get(section, 'pass')
+            repos.append(SvnNotifier(section, server, user, passw))
+            print 'Monitoring SVN repository %s: %s' % (section, server)
     except BaseException as e:
         print "Error while parsing file '%s':" % config_file
         print e
         exit()
-
-def notify(paths, authors, messages, numbers):
-    """Display the changed paths using libnotify"""
-    title_string = 'New commits to repository'
-    path_string = ', '.join(paths)
-    author_string = ', '.join(authors)
-    number_string = ', '.join(numbers)
-    message_string = path_string + ' ' + number_string
-    
-    if pynotify.init("SVN Monitor"):
-        n = pynotify.Notification(title_string, message_string, "emblem-shared")
-        n.show()
-
-def log_message(paths, authors, messages, revisions):
-    """Print a log message containing the time, authors, paths, and messages"""
-    now = datetime.datetime.now()
-    now = now.strftime("%H:%M")
-    path_string = ', '.join(paths)
-    author_string = ', '.join(authors)
-    message_string = ', '.join(messages)
-    number_string = ', '.join(revisions)
-    print "[%s] Paths: %s -- Authors: %s -- Revisions: %s -- Messages: %s" % (now, path_string, author_string, number_string, message_string)
-    
-
-def credentials(realm, username, may_save):
-    """Return the default login credentials"""
-    return True, svn_username, svn_password, False
-
-def discover_changes(last_revision=pysvn.Revision(pysvn.opt_revision_kind.number, 0)):
-    """Find out the changes occured since the last time this method is ran"""
-    if last_revision is None:
-        last_revision=pysvn.Revision(pysvn.opt_revision_kind.number, 18180)
-    client = pysvn.Client()
-    client.callback_get_login = credentials
-    log = client.log(
-        svn_root, 
-        discover_changed_paths=True,
-        revision_end=last_revision
-        )
-    if len(log) is 1:
-        return last_revision, None, None, None, None
-    authors = []
-    paths = []
-    messages = []
-    revisions = []
-    for entry in log[:-1]:
-        if entry.author not in authors:
-            authors.append(entry.author)
-        for change in entry.changed_paths:
-            path = change.path #.split('/',2)[1]
-            if path not in paths:
-                paths.append(path)
-        messages.append(entry.message)
-        revisions.append("\nhttps://hub.boxuk.com/projects/amaxus4custom/repository/revisions/" + str(entry.revision.number))
-    last_revision = log[0].revision
-        
-    return last_revision, authors, paths, messages, revisions
+    pynotify.init("SVN Monitor")
+    print '- Press %s to quit -' % '^C'
+    while True:
+        for repo in repos:
+            repo.discover_changes()
+        time.sleep(60 * 10)  # Every 10 minutes
 
 if __name__ == '__main__':
-    read_config()
-    print 'Monitoring SVN repository: %s' % svn_root
-    print '- Press %s to quit -' % '^C'
-    last_revision = None
-    while True:
-        last_revision, authors, paths, messages, revisions = discover_changes(last_revision)
-        if paths is not None:
-            log_message(paths, authors, messages, revisions)
-            notify(paths, authors, messages, revisions)
-        time.sleep(30)
+    main()
