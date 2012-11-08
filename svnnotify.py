b83ff8e55059ab702f397d0f85262bc8782fe8d3
Markus Graube <mgraube@mgraube-Studio-XPS-1645.(none)>
2012-11-08 12:10:29 +0100

    - Notification of paths as list - use of logging mechanism - storage of last revision in configuration file

--- svnnotify.py
+++ svnnotify.py
@@ -1,40 +1,51 @@
 #!/usr/bin/python
 #
-# script to notify the user for changes in a subversion repository.
+# Script to notify the user for changes in a subversion repository.
 #
 # Depends on a configuration file with one or more of the following entries:
 #
-#   [repo-name]
+#   [ServerName]
 #   server=SVN_REPO_TO_MONITOR
 #   user=YOUR_SVN_USERNAME
 #   pass=YOUR_SVN_PASSWORD
 #
 
 try:
-    import pysvn, pynotify
+    import pynotify
+    from pysvn import Client, Revision, opt_revision_kind, ClientError
 except ImportError:
     print "Error while loading external depencencies."
     print "Make sure 'pysvn' and 'pynotify' are installed."
     exit()
 
-import datetime, time, os, ConfigParser as cfg
+import datetime, time, os, ConfigParser as cfg, logging
+
 
 class SvnNotifier():
 
-    def __init__(self, name, svn_root, svn_username, svn_password):
+    def __init__(self, name, svn_root, svn_username, svn_password, parser, config_file):
         self.name = name
         self.svn_root = svn_root
         self.svn_username = svn_username
         self.svn_password = svn_password
-        self.last_revision = pysvn.Revision(pysvn.opt_revision_kind.number, 0)
-        self.client = pysvn.Client()
+        self.client = Client()
         self.client.callback_get_login = self.credentials
+        self.parser = parser
+        self.config_file = config_file
 
     def notify(self, author, date, revision, message, paths):
         """Display the changed paths using libnotify"""
         title_string = 'New commit #%s to repository %s' % (revision, self.name)
-        message_string = "[%s] %s: %s\n Paths: %s" % \
-                        (date.strftime("%d.%m %H:%M"), author, message, paths)
+        message_string = "[%s] %s\n <i>%s</i>\n" % \
+                        (date.strftime("%d.%m %H:%M"), author, message)
+        message_string += "<ul>"
+        for p in paths[:5]:
+            if len(p)>50:
+                p = "...%s" % p[-50:]
+            message_string += "<li>%s</li>" % p
+        if len(paths)>6:
+            message_string += "<li>...</li>"
+        message_string += "</ul>"
         pynotify.Notification(title_string, message_string, "view-refresh").show()
 
     def credentials(self, realm, username, may_save):
@@ -45,17 +56,23 @@ class SvnNotifier():
         """
         Find out the changes occured since the last time this method is ran
         """
+        logging.debug("Discover Changes in %s" % self.name)
+        try:
+            self.parser.read(self.config_file)
+            last_revision = self.parser.get(self.name, "last_revision")
+        except cfg.NoOptionError:
+            last_revision = 0
         log = self.client.log(
             self.svn_root,
             discover_changed_paths=True,
-            revision_end=self.last_revision
+            revision_end=Revision(opt_revision_kind.number, last_revision)
             )
         log = log[:-1]   # Ignore last revision
         if len(log) > 0:
-            self.last_revision = log[0].revision
-            if len(log) > 3:
-                pynotify.Notification("Older Commits for repository %s available" % self.name, None, "view-refresh").show()
-                log = log[0:3]
+            # update last revision in config file
+            last_revision = log[0].revision.number
+            self.parser.set(self.name, "last_revision", last_revision)
+            self.parser.write(open(self.config_file, 'w'))
             log.reverse()
             for entry in log:
                 author = entry.get('author')
@@ -67,10 +84,16 @@ class SvnNotifier():
                     if change.path not in paths:
                         paths.append(change.path)
                 self.notify(author, date, rev, message, paths)
+        else:
+            logging.info("No new commits for %s" % self.name)
 
 
 def main():
     path = os.path.dirname(__file__)
+    logging.basicConfig(
+        level = logging.INFO,
+        format = "%(asctime)s %(levelname)s: %(message)s",
+        datefmt = "%d.%m.%Y %H:%M:%S")
     config_file = os.path.join(path, 'svnnotify.cfg')
     repos = []
     try:
@@ -80,18 +103,23 @@ def main():
             server = parser.get(section, 'server')
             user = parser.get(section, 'user')
             passw = parser.get(section, 'pass')
-            repos.append(SvnNotifier(section, server, user, passw))
-            print 'Monitoring SVN repository %s: %s' % (section, server)
-    except BaseException as e:
-        print "Error while parsing file '%s':" % config_file
-        print e
+            repos.append(SvnNotifier(section, server, user, passw, parser, config_file))
+            logging.info('Monitoring SVN repository %s (%s)' % (section, server))
+    except BaseException as exception:
+        logging.critical("Error while parsing file '%s':" % config_file)
+        logging.critical(exception)
         exit()
     pynotify.init("SVN Monitor")
-    print '- Press %s to quit -' % '^C'
+
     while True:
         for repo in repos:
-            repo.discover_changes()
-        time.sleep(60 * 10)  # Every 10 minutes
+            try:
+                repo.discover_changes()
+            except ClientError as e:
+                print e
+                logging.error("Repo %s not accessible" % repo.name)
+                logging.error(e)
+        time.sleep(60 * 5)  # Every 5 minutes
 
 if __name__ == '__main__':
     main()
