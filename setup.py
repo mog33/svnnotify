10545ffbfff1b57a94c6b929ee5eccb0d20f0d86
mog33 <ctrl.alt.del@gmx.net>
2013-07-29 23:07:48 +0200

    debian build changed to dh

--- setup.py
+++
@@ -1,9 +0,0 @@
-from distutils.core import setup
-setup(name='svnnotify',
-      version='0.1',
-      author='Markus Graube',
-      author_email='markus.graube@tu-dresden.de',
-      description='Notify about changes in svn repositories',
-      requires=['appdirs','pysvn','pynotify'],
-      py_modules=['svnnotify']
-      )
