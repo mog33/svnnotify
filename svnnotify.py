ff1776147b9034b9f1c8520eb7fa061d7d59c962
mog33 <ctrl.alt.del@gmx.net>
2013-09-04 08:49:19 +0200

    small changes

--- svnnotify.py
+++ svnnotify.py
@@ -14,18 +14,19 @@ import pynotify
 import datetime, time, os, logging
 from pysvn import Client, Revision, opt_revision_kind, ClientError
 from appdirs import user_data_dir
+from ConfigParser import ConfigParser
 
 
-class SvnNotifier():
+class SvnRepoMonitor():
 
-    def __init__(self, name, svn_root, svn_username, svn_password, parser, config_file):
+    def __init__(self, name, svn_root, svn_username, svn_password, config_file):
         self.name = name
         self.svn_root = svn_root
         self.svn_username = svn_username
         self.svn_password = svn_password
         self.client = Client()
         self.client.callback_get_login = self.credentials
-        self.parser = parser
+        self.parser = ConfigParser()
         self.config_file = config_file
 
     def notify(self, author, date, revision, message, paths):
@@ -41,7 +42,7 @@ class SvnNotifier():
         if len(paths)>6:
             message_string += "<li>...</li>"
         message_string += "</ul></p>"
-        logging.debug("Open pynotify.Notification: %s | %s" % (title_string,message_string))
+        logging.debug("Open pynotify.Notification: %s | %s" % (title_string, message_string))
         pynotify.Notification(title_string, message_string, "view-refresh").show()
 
     def credentials(self, realm, username, may_save):
@@ -71,7 +72,7 @@ class SvnNotifier():
             self.parser.set(self.name, "last_revision", last_revision)
             self.parser.write(open(self.config_file, 'w'))
             log.reverse()
-            if len(log)>5: # Show only last 10 commits
+            if len(log)>5: # Show only most recent commits
                 pynotify.Notification("Even more commits for repository %s" % self.name, "There are %s more new commits in the repository" % (len(log)-5), "view-refresh").show()
                 log = log [-5:]
             for entry in log:
@@ -81,47 +82,41 @@ class SvnNotifier():
                 date = datetime.datetime.fromtimestamp(entry.date)
                 paths = []
                 for change in entry.changed_paths:
-                    if change.path not in paths:
-                        paths.append(change.path)
+                    paths.append(change.path)
                 self.notify(author, date, rev, message, paths)
         else:
-            logging.info("No new commits for %s" % self.name)
+            logging.debug("No new commits for %s" % self.name)
 
 
-class Configuration():
+class SvnNotifier():
 
-    def __init__(self, config_file):
+    def __init__(self, config_file, interval):
+        logging.basicConfig(
+            level = logging.INFO,
+            format = "%(asctime)s %(levelname)s: %(message)s",
+            datefmt = "%d.%m.%Y %H:%M:%S",
+            #filename= os.path.join(os.getcwd(), 'svnnotify.log')
+            )
+        self.interval = interval
         if config_file:
             self.config_file = config_file
         else:
             self.config_file = self.get_config_file()
-        logging.info("SvnNotify started with configuration file %s" % config_file)
+        self.repos = self.create_repos_from_svn_config()
+        pynotify.init("SVN Monitor")
+        logging.info("SvnNotifier created with configuration file %s monitoring %i repositories." % (self.config_file, len(self.repos)))
 
-    def get_config_file(self):
-        cfg = os.path.join(os.getcwd(),"svnnotify.cfg")
-        if os.path.isfile(cfg):
-            return cfg
-        cfg = os.path.join(user_data_dir("svnnotify"),"svnnotify.cfg")
-        if os.path.isfile(cfg):
-            return cfg
-        else:
-            if not os.path.exists(user_data_dir("svnnotify")):
-                os.makedirs(user_data_dir("svnnotify"))
-            f = open(cfg, "w")
-            f.write("""#[Repo1Name]
-#server = https://server1.com/svn/repo
-#user = username
-#pass = password
-
-#[Repo2Name]
-#server = svn+ssh://username@server2.com/svn/repo""") 
-            print("Configuration file for svnnotify created: %s" % cfg)
-            print("Modify it to your needs before restarting")
-            exit()
-
-
+    def run(self):
+        while True:
+            for repo in self.repos:
+                try:
+                    repo.discover_changes()
+                except ClientError as e:
+                    logging.error("Repo %s not accessible\n%s" % (repo.name,e))
+            time.sleep(self.interval)
+    
     def create_repos_from_svn_config(self):
-        from ConfigParser import ConfigParser
+        logging.info("Reading configuration file %s" % self.config_file)
         parser = ConfigParser()
         parser.read(self.config_file)
         repos = []
@@ -139,41 +134,31 @@ class Configuration():
                 passw = parser.get(section, 'pass')
             else:
                 passw = None
-            repos.append(SvnNotifier(section, server, user, passw, parser, self.config_file))
+            repos.append(SvnRepoMonitor(section, server, user, passw, self.config_file))
             logging.info('Monitoring SVN repository %s (%s)' % (section, server))
         if repos:
             return repos
         else:
-            print("No sections in configuration file found. Aborting")
+            logging.error("No sections in configuration file found. Aborting")
             exit()
 
-
-def svn_notify(config_file, interval):
-    path = os.path.dirname(__file__)
-    logging.basicConfig(
-        level = logging.INFO,
-        format = "%(asctime)s %(levelname)s: %(message)s",
-        datefmt = "%d.%m.%Y %H:%M:%S",
-        #filename= os.path.join(path, 'svnnotify.log')
-        )
-    conf = Configuration(config_file)
-    repos = conf.create_repos_from_svn_config()
-    pynotify.init("SVN Monitor")
-    pynotify.Notification("SvnNotify started", "SvnNotify started with configuration file %s monitoring %i repositories." % (conf.config_file, len(repos)), "view-refresh").show()
-    while True:
-        for repo in repos:
-            try:
-                repo.discover_changes()
-            except ClientError as e:
-                logging.error("Repo %s not accessible\n%s" % (repo.name,e))
-        time.sleep(interval)
+    def get_config_file(self):
+        cfg = os.path.join(os.getcwd(),"svnnotify.cfg")
+        if os.path.isfile(cfg):
+            return cfg
+        cfg = os.path.join(user_data_dir("svnnotify"),"svnnotify.cfg")
+        if os.path.isfile(cfg):
+            return cfg
+        logging.error("No configuration file found. Aborting")
+        exit()
 
 
 if __name__ == '__main__':
     from argparse import ArgumentParser, FileType
     parser = ArgumentParser(description="Notifies about changes in SVN repositories")
     parser.add_argument("-c","--config", help="path to configuration file (default svnnotify.cfg in working directory or in %s)" % user_data_dir("svnnotify"))
-    parser.add_argument("-v","--version", help="shows version of svnnotify", action='version', version='%(prog)s 0.3')
+    parser.add_argument("-v","--version", help="shows version of svnnotify", action='version', version='%(prog)s 0.4')
     parser.add_argument("-i","--interval", help="update interval in seconds (default 5 minutes)", type=int, default=60*5)
     args = parser.parse_args()
-    svn_notify(args.config, args.interval)
+    svn_notifier = SvnNotifier(args.config, args.interval)
+    svn_notifier.run()
